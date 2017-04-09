f90d8a3c574ed4771fc31ea7b6ea40f4632ff77a
Erik <erikdesjardins@users.noreply.github.com>
2017-04-09 19:33:04 -0400

    temp: disable firefox deployment

--- deploy.js
+++ deploy.js
@@ -5,14 +5,14 @@
 const fs = require('fs');
 const path = require('path');
 const deployChrome = require('chrome-extension-deploy');
-const deployFirefox = require('firefox-extension-deploy');
-const manifest = require('./dist/manifest.json');
+// const deployFirefox = require('firefox-extension-deploy');
+// const manifest = require('./dist/manifest.json');
 
 deployChrome({
 	clientId: process.env.CHROME_CLIENT_ID,
 	clientSecret: process.env.CHROME_CLIENT_SECRET,
 	refreshToken: process.env.CHROME_REFRESH_TOKEN,
-	id: 'obbjmeopodlheliibjoabgbmchpecjaj',
+	id: 'TODO',
 	zip: fs.readFileSync(path.join(__dirname, 'dist/BTD2X.zip')),
 }).then(() => {
 	console.log('Chrome deploy complete!');
@@ -21,7 +21,7 @@ deployChrome({
 	process.exitCode = 1;
 });
 
-deployFirefox({
+/* deployFirefox({
 	issuer: process.env.FIREFOX_ISSUER,
 	secret: process.env.FIREFOX_SECRET,
 	id: manifest.applications.gecko.id,
@@ -32,4 +32,4 @@ deployFirefox({
 }, err => {
 	console.error('Firefox failed:', err);
 	process.exitCode = 1;
-});
+}); */
