cf3d15499f423e6c22acdaa1b6695d8b34ad8b48
Erik <erikdesjardins@users.noreply.github.com>
2017-04-09 17:38:25 -0400

    deploy script

---
+++ deploy.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const deployChrome = require('chrome-extension-deploy');
+const deployFirefox = require('firefox-extension-deploy');
+const manifest = require('./dist/manifest.json');
+
+deployChrome({
+	clientId: process.env.CHROME_CLIENT_ID,
+	clientSecret: process.env.CHROME_CLIENT_SECRET,
+	refreshToken: process.env.CHROME_REFRESH_TOKEN,
+	id: 'obbjmeopodlheliibjoabgbmchpecjaj',
+	zip: fs.readFileSync(path.join(__dirname, 'dist/BTD2X.zip'))
+}).then(function() {
+	console.log('Chrome deploy complete!');
+}, function(err) {
+	console.error('Chrome failed:', err);
+	process.exitCode = 1;
+});
+
+deployFirefox({
+	issuer: process.env.FIREFOX_ISSUER,
+	secret: process.env.FIREFOX_SECRET,
+	id: manifest.applications.gecko.id,
+	version: manifest.version,
+	src: fs.createReadStream(path.join(__dirname, 'dist/BTD2X.zip'))
+}).then(function() {
+	console.log('Firefox deploy complete!');
+}, function(err) {
+	console.error('Firefox failed:', err);
+	process.exitCode = 1;
+});
