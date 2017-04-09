c652a953b8e351746bbf647ba88e9f2983fa2597
Erik <erikdesjardins@users.noreply.github.com>
2017-04-09 18:30:45 -0400

    basic js setup

---
+++ src/js/browser/_wrapper.js
@@ -0,0 +1,12 @@
+export function wrapSingleResult(fn) {
+	return (...args) =>
+		new Promise((resolve, reject) =>
+			fn(...args, (result) => {
+				if (chrome.runtime.lastError) {
+					reject(new Error(chrome.runtime.lastError.message));
+				} else {
+					resolve(result);
+				}
+			})
+		);
+}
---
+++ src/js/browser/storage.js
@@ -0,0 +1,13 @@
+import { wrapSingleResult } from './_wrapper';
+
+const _get = wrapSingleResult((keys, callback) => chrome.storage.sync.get(keys, callback));
+
+export async function get(key, defaultVal = null) {
+	return (await _get({ [key]: defaultVal }))[key];
+}
+
+const _set = wrapSingleResult((items, callback) => chrome.storage.sync.set(items, callback));
+
+export async function set(key, val) {
+	await _set({ [key]: val });
+}
