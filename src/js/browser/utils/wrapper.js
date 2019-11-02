0df15d9287457d4d45fc41ffcc5d14b755e2c6da
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2019-11-02 17:57:29 -0400

    update deps

--- src/js/browser/utils/wrapper.js
+++ src/js/browser/utils/wrapper.js
@@ -1,14 +1,11 @@
 /* @flow */
 
 export function wrapSingleResult(fn: (...args: mixed[]) => void): (...args: mixed[]) => Promise<any> {
-	return (...args) =>
-		new Promise((resolve, reject) =>
-			fn(...args, result => {
-				if (chrome.runtime.lastError) {
-					reject(new Error(chrome.runtime.lastError.message));
-				} else {
-					resolve(result);
-				}
-			})
-		);
+	return (...args) => new Promise((resolve, reject) => fn(...args, result => {
+		if (chrome.runtime.lastError) {
+			reject(new Error(chrome.runtime.lastError.message));
+		} else {
+			resolve(result);
+		}
+	}));
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,42 @@ const path = require('path');
 
 const InertEntryPlugin = require('inert-entry-webpack-plugin');
 const ZipPlugin = require('zip-webpack-plugin');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
-module.exports = ({ production = false, zip = false } = {}) => ({
-	entry: 'extricate-loader!interpolate-loader!./src/manifest.json',
-	bail: production,
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'manifest.json',
-	},
-	module: {
-		rules: [{
-			test: /\.entry\.js$/,
-			use: [
-				{ loader: 'file-loader', options: { name: '[name].js' } },
-				{ loader: 'webpack-rollup-loader' },
-			],
-		}, {
-			test: /\.js$/,
-			use: [
-				{ loader: 'babel-loader' },
-			],
-		}, {
-			test: /\.(png)$/,
-			use: [
-				{ loader: 'file-loader', options: { name: '[name].[ext]' } },
-			],
-		}],
-	},
-	plugins: [
-		new ProgressBarPlugin(),
-		new InertEntryPlugin(),
-		(zip && new ZipPlugin({ filename: 'BTD2X.zip' })),
-	].filter(x => x),
-});
+module.exports = (env = {}, argv = {}) => {
+	const zip = env.zip || false;
+	const production = argv.mode === 'production';
+	return {
+		entry: 'extricate-loader!interpolate-loader!./src/manifest.json',
+		output: {
+			path: path.join(__dirname, 'dist'),
+			filename: 'manifest.json',
+		},
+		module: {
+			rules: [{
+				test: /\.entry\.js$/,
+				use: [
+					{ loader: 'file-loader', options: { name: '[name].js' } },
+					{ loader: 'webpack-rollup-loader' },
+				],
+			}, {
+				test: /\.js$/,
+				use: [
+					{ loader: 'babel-loader', options: { comments: !production } },
+				],
+			}, {
+				test: /\.(png)$/,
+				use: [
+					{ loader: 'file-loader', options: { name: '[name].[ext]' } },
+				],
+			}],
+		},
+		optimization: {
+			minimize: false,
+			concatenateModules: true,
+		},
+		plugins: [
+			new InertEntryPlugin(),
+			(zip && new ZipPlugin({ filename: 'BTD2X.zip' })),
+		].filter(x => x),
+	};
+};
